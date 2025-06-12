fa8590e5afba210524e7898248205c950ac1240c
Hnyumin <163375851+Hnyumin@users.noreply.github.com>
2025-06-12 12:35:10 +0900

    Update garden.js

--- js/garden.js
+++ js/garden.js
@@ -31,10 +31,19 @@ function setup() {
 
   wishes = getWishes();
   let flowerCount = wishes.length || 20;
-  for (let i = 0; i < flowerCount; i++) {
-    let x = map(i, 0, flowerCount - 1, 80, width - 80);
-    availableX.push(x);
-  }
+ let margin = 80;
+let lastX = margin;
+
+for (let i = 0; i < flowerCount; i++) {
+  let gap = random(40, 100); // 간격을 40~100 사이로 랜덤 설정
+  let x = lastX + gap;
+
+  // 너무 오른쪽으로 나가면 멈춰
+  if (x > width - margin) break;
+
+  availableX.push(x);
+  lastX = x;
+}
   currentIndex = 0;
 }
 
